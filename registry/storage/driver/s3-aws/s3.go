cbb01b4c6aa74f916ec2eda9f02abcb4ffa23857
Sebastiaan van Stijn <github@gone.nl>
2022-11-02 23:31:23 +0100

    use http consts for request methods
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -1141,13 +1141,11 @@ ListLoop:
 // URLFor returns a URL which may be used to retrieve the content stored at the given path.
 // May return an UnsupportedMethodErr in certain StorageDriver implementations.
 func (d *driver) URLFor(ctx context.Context, path string, options map[string]interface{}) (string, error) {
-	s := d.s3Client(ctx)
-
-	methodString := "GET"
+	methodString := http.MethodGet
 	method, ok := options["method"]
 	if ok {
 		methodString, ok = method.(string)
-		if !ok || (methodString != "GET" && methodString != "HEAD") {
+		if !ok || (methodString != http.MethodGet && methodString != http.MethodHead) {
 			return "", storagedriver.ErrUnsupportedMethod{}
 		}
 	}
@@ -1164,13 +1162,13 @@ func (d *driver) URLFor(ctx context.Context, path string, options map[string]int
 	var req *request.Request
 
 	switch methodString {
-	case "GET":
-		req, _ = s.GetObjectRequest(&s3.GetObjectInput{
+	case http.MethodGet:
+		req, _ = d.S3.GetObjectRequest(&s3.GetObjectInput{
 			Bucket: aws.String(d.Bucket),
 			Key:    aws.String(d.s3Path(path)),
 		})
-	case "HEAD":
-		req, _ = s.HeadObjectRequest(&s3.HeadObjectInput{
+	case http.MethodHead:
+		req, _ = d.S3.HeadObjectRequest(&s3.HeadObjectInput{
 			Bucket: aws.String(d.Bucket),
 			Key:    aws.String(d.s3Path(path)),
 		})
